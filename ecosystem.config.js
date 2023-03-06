2de7c929c3f48eb65304a96772f7a85b8626f857
Benicio Cardozo <beniciocardozomdp@gmail.com>
2023-03-05 21:54:27 -0300

    fix: add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'Referrals',
-      port: '3000',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs'
-    }
-  ]
-}
+      name: "Referrals",
+      port: "3000",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
+      env: {
+        ORIGIN: "https://referfortech.site",
+        NUXT_LINKEDIN_CLIENT_ID: "783x38dt7lt81e",
+        NUXT_LINKEDIN_CLIENT_SECRET: "CJuIEakgEoxxVntF",
+        NUXT_DATABASE_URL:
+          "postgres://postgres:new_password@localhost:5432/referrals",
+        NUXT_LINKEDIN_PASSWORD: "Benicapo2",
+        NUXT_LINKEDIN_EMAIL: "beniciocardozomdp@gmail.com",
+      },
+    },
+  ],
+};
